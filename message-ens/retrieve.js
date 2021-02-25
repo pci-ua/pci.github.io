7b6342e150df2bb975b19998e3875b4ffe12b656
StrategFirst <mathieu.toulon@free.fr>
2021-02-25 01:20:04 +0100

    Add the DOM purifier

--- message-ens/retrieve.js
+++ message-ens/retrieve.js
@@ -2,14 +2,15 @@ window.addEventListener('load' , () => {
     fetch('https://pci.kher.nl/messages.php')
       .then( r => r.json() )
       .then( obj => {
-
-        console.log(obj);
         if(obj.status === "ok") {
           obj.messages.pop();
           let target = document.querySelector('main');
           for(let msg of obj.messages) {
             let div = document.createElement('div');
-            div.innerHTML = `<h4>${msg.nom.toUpperCase()} ${msg.prenom}</h4><p>${msg.message}</p>`;;
+            let nom = DOMPurify.sanitize( msg.nom.toUpperCase() );
+            let prenom = DOMPurify.sanitize( msg.prenom );
+            let message = DOMPurify.sanitize( msg.message );
+            div.innerHTML = `<h4>${nom} ${prenom}</h4><p>${message}</p>`;;
             div.classList.add('message');
             target.appendChild(div);
           }
