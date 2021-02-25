abdc13ae370e0811f507a9a487773aab3a486680
StrategFirst <mathieu.toulon@free.fr>
2021-02-25 01:11:37 +0100

    remove useless <pre> tag

--- message-ens/retrieve.js
+++ message-ens/retrieve.js
@@ -9,7 +9,7 @@ window.addEventListener('load' , () => {
           let target = document.querySelector('main');
           for(let msg of obj.messages) {
             let div = document.createElement('div');
-            div.innerHTML = `<h4>${msg.nom.toUpperCase()} ${msg.prenom}</h4><p><pre>${msg.message}</pre></p>`;;
+            div.innerHTML = `<h4>${msg.nom.toUpperCase()} ${msg.prenom}</h4><p>${msg.message}</p>`;;
             div.classList.add('message');
             target.appendChild(div);
           }
