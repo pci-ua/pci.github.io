fe5bbd70fb998cfff810a8defa328e2c243cc38e
StrategFirst <mathieu.toulon@free.fr>
2021-02-25 01:09:06 +0100

    Basic structure to retrieve data

--- message-ens/retrieve.js
+++ message-ens/retrieve.js
@@ -1,8 +1,21 @@
 window.addEventListener('load' , () => {
-  document.body.addEventListener('load' , () => {
     fetch('https://pci.kher.nl/messages.php')
       .then( r => r.json() )
-      .then( console.log )
-      .catch( console.error )
-  });
+      .then( obj => {
+
+        console.log(obj);
+        if(obj.status === "ok") {
+          obj.messages.pop();
+          let target = document.querySelector('main');
+          for(let msg of obj.messages) {
+            let div = document.createElement('div');
+            div.innerHTML = `<h4>${msg.nom.toUpperCase()} ${msg.prenom}</h4><p><pre>${msg.message}</pre></p>`;;
+            div.classList.add('message');
+            target.appendChild(div);
+          }
+        } else {
+          throw obj;
+        }
+      } )
+      .catch( console.error );
 });
