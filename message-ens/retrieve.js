b809c86139a4666e6fb13727634efcc36b974605
StrategFirst <mathieu.toulon@free.fr>
2021-02-25 01:36:53 +0100

    Keep line break

--- message-ens/retrieve.js
+++ message-ens/retrieve.js
@@ -7,7 +7,7 @@ window.addEventListener('load' , () => {
           let target = document.querySelector('main');
           for(let msg of obj.messages) {
             let div = document.createElement('div');
-            let nom = DOMPurify.sanitize( msg.nom.toUpperCase() );
+            let nom = DOMPurify.sanitize( msg.nom.toUpperCase() ).replace(/\n+/ig,'<br/>');
             let prenom = DOMPurify.sanitize( msg.prenom );
             let message = DOMPurify.sanitize( msg.message );
             div.innerHTML = `<h4>${nom} ${prenom}</h4><p>${message}</p>`;;
