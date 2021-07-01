1e7ebe8417abd890bb895c53e66a4a04f6cbc79c
ChipChoped <chaimaa-touhami@outlook.fr>
2021-07-01 12:23:10 +0200

    Adresse fetch modifier pour fonctionner en ligne et en local

--- assets/footer/footer.js
+++ assets/footer/footer.js
@@ -1,6 +1,10 @@
 window.addEventListener( 'load' , async () => {
   let element = document.createElement('footer');
-  let content = await fetch(`${document.location.origin}/assets/footer/footer.html`).then( reponse => reponse.text() );
+  //               Ancienne commande ne fonctionnant pas en local
+  //let content = await fetch(`${document.location.origin}/assets/footer/footer.html`).then( reponse => reponse.text() );
+
+  //               Nouvelle commande fonctionnant aussi en local
+  let content = await fetch(`assets/footer/footer.html`).then( reponse => reponse.text() );
   element.innerHTML = content;
   document.body.appendChild(element);
 });
