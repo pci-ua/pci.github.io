f65aef647f25b0a87fd3bd7896ffc63352f0baed
ChipChoped <chaimaa-touhami@outlook.fr>
2021-08-16 15:51:27 +0200

    Update footer.js

--- assets/footer/footer.js
+++ assets/footer/footer.js
@@ -1,6 +1,6 @@
 window.addEventListener( 'load' , async () => {
   let element = document.createElement('footer');
-  let content = await fetch(`assets/footer/footer.html`).then( reponse => reponse.text() );
+  let content = await fetch(`${document.location.origin}/assets/footer/footer.html`).then( reponse => reponse.text() );
   element.innerHTML = content;
   document.body.appendChild(element);
 });
